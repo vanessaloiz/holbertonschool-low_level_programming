599d6d6d9df2fabff2bec36d08da16a1cc1e2401
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-02 16:36:19 +0000

    4.Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-include
+#include<stdio.h>
 /**
  * main - void
  * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle")
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
