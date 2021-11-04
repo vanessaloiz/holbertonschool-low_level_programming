b9f9222e2bc36c1422661a3bf4a181bb57716caf
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-04 19:26:44 +0000

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main- Main block print the alpahbet
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
