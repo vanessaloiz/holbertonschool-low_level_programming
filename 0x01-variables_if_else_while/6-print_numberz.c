0dafbf559d5fd34e079c96a3fb4fda4a71322937
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-04 20:06:53 +0000

    6.Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Main block Numberz
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
