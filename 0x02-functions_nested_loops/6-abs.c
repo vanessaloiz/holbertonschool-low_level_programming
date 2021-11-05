1e88e913f9cfabde298f95992c9bd5d246de85ef
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-05 05:59:07 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _abs - Computes the absolute value
+ * @n: Variable
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
