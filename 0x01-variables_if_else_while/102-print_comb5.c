8c56e5c3148d9050367ecead959ebd9bc79fb562
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-05 02:37:01 +0000

    print number very difficult

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - Prints all combination of 2 digits
+ * Return: 0
+ */
+int main(void)
+{
+	int n, n1, n2, n3;
+	int p1, p2;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (n1 = '0'; n1 <= '8'; n1++)
+		{
+			for (n2 = n; n2 <= '9'; n2++)
+			{
+				for (n3 = '0'; n3 <= '9'; n3++)
+				{
+					p1 = n + n1;
+					p2 = n2 + n3;
+					if (p2 > p1)
+					{
+						putchar(n);
+						putchar(n1);
+						putchar(' ');
+						putchar(n2);
+						putchar(n3);
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
