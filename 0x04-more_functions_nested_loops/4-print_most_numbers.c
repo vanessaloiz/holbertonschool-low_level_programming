ee8572bd3635b377abb7ece290ffb71a3fd6601f
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-07 18:06:50 +0000

    Prints numbers except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * print_most_numbers - From 0-9 except 2 and 4.
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+		if (c != 50 && c != 52)
+			_putchar(c);
+	_putchar('\n');
+}
