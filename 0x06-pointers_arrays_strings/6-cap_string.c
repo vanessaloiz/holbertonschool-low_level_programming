b754030ac02ee975a0ce68fb9a51bad778766fca
vanessaloiz <vanessaloiz7794@gmail.com>
2021-11-22 04:42:31 +0000

    Cap string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,11 +7,13 @@
 char *cap_string(char *str)
 {
 	int idx;
-
+	
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
 	for (idx = 0; str[idx] != '\0'; idx++)
 	{
-		if (str[idx] == 32 || str[idx] == '\t' || +
-			str[idx] == '\n' || str[idx] == 43 || +
+		if (str[idx] == 32 || str[idx] == 9 || +
+			str[idx] == 10 || str[idx] == 44 || +
 			str[idx] == 33 || str[idx] == 34 || +
 			str[idx] == 40 || str[idx] == 41 || +
 			str[idx] == 59 || str[idx] == 63 || +
